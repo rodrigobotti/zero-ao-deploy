e9666fd6cb64715a44903289e4c549eecf2b5130
Rodrigo Botti <rodrigo.botti@gmail.com>
2019-11-25 15:29:09 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+const index = ctx => {
+  ctx.body = 'Node.JS do Zero ao Deploy'
+}
+
+const hello = ctx => {
+  const name = ctx.params.name || 'World'
+  ctx.body = `Hello ${name}!`
+}
+
+router.get('/', index)
+router.get('/hello', hello)
+router.get('/hello/:name', hello)
+
+app.use(router.routes())
+
+const port = 3000
+
+app.listen(port)
+  .on('listening', () => console.log(`Listening on port ${port}`))
